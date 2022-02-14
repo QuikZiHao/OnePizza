b78a3f847f6bc46eb2042458c53c0e00c3ec84a4
Quik Zi Hao <94067315+QuikZiHao@users.noreply.github.com>
2022-02-14 23:44:30 +0800

    Update One Pizza.py

--- One Pizza.py
+++ One Pizza.py
@@ -10,7 +10,8 @@
 # ——————————————————————————————————————————————————————————————————————————————————————————————————————————#
 
 
-filename ="e_elaborate"   #input ur input file here
+import random
+filename ="input_file\d_difficult"   #input ur input file here
 f = open(filename+".in.txt", 'r')
 file = f.readlines()
 Nppl = int(file[0])
@@ -18,35 +19,43 @@ book = []
 menu = set()
 newmenu = set()
 customer = 0
+finallyCustomer = 0
+finallyMenu = set()
 for i in range(0,Nppl):
     like = file[2*i+1].split()
     like = like[1:]
     dislike = file[2*i+2].split()
     dislike = dislike[1:]
-
     new = [set(like),set(dislike)]
     book.append(new)
+for times in range(20):
+    menu = set()
+    newmenu = set()
+    customer = 0
+    for i in range(0,Nppl):
+        #print('newmenu before', newmenu)
+        newmenu = menu.union(book[i][0])
+        #print('newmenu now', newmenu)
+        newmenu = newmenu-set(book[i][1])
+        newmenucus = 0
+        #print('newmenu after',newmenu,i)
 
-    #print('newmenu before', newmenu)
-    newmenu = menu.union(set(like))
-    #print('newmenu now', newmenu)
-    newmenu = newmenu-set(dislike)
-    newmenucus = 0
-    #print('newmenu after',newmenu,i)
+        for k in range(0,Nppl):
+            if book[k][0] <= newmenu and book[k][1]&newmenu == set():
+                newmenucus += 1
 
-    for k in range(0,len(book)):
-        if book[k][0] <= newmenu and book[k][1]&newmenu == set():
-            newmenucus += 1
-
-    if newmenucus>customer:
-        customer = newmenucus
-        menu = newmenu
-
-output = str(len(menu))
-menu = list(menu)
-str1 = ' '.join(menu)
+        if newmenucus>customer:
+            customer = newmenucus
+            menu = newmenu
+    if(customer>finallyCustomer):
+        finallyCustomer = customer
+        finallyMenu = menu
+    print(times,customer)
+    random.shuffle(book)
+output = str(len(finallyMenu))
+menu = list(finallyMenu)
+str1 = ' '.join(finallyMenu)
 output = output+" "+str1
-
 a_file = open(filename+".out.txt", "x")
 a_file.write(output)
 a_file.close()
